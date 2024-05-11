5ede3032780cc1350da13af8d99b9a206d2a2bf7
samuel6814 <71970320+samuel6814@users.noreply.github.com>
2024-05-11 09:21:36 +0000

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react"
 import styles from "./footer.module.css"
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image"
+import Link from "next/link"
 
 const Footer = () => {
   return (
