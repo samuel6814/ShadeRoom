70c7150b3eda1979e881434fc339ff4e4fdf1243
samuel6814 <71970320+samuel6814@users.noreply.github.com>
2024-05-11 08:39:28 +0000

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,11 @@
+import React from "react";
 import "./globals.css"
 import { Inter } from 'next/font/google';
-import Navbar from "@/components/navbar/Navbar";
-import Footer from "@/components/footer/Footer";
-import { ThemeContextProvider } from "@/context/ThemeContext";
-import ThemeProvider from "@/providers/ThemeProvider";
-import AuthProvider from "@/providers/AuthProvider";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer"
+import { ThemeContextProvider } from "../context/ThemeContext";
+import ThemeProvider from "../providers/ThemeProvider";
+import AuthProvider from "../providers/AuthProvider";
 
 const inter = Inter({ subsets: ['latin'] })
 
