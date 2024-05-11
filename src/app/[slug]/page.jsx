c4c5381ede4dd140bb0f5b0ffc9eccc21b28f870
samuel6814 <71970320+samuel6814@users.noreply.github.com>
2024-05-11 08:34:57 +0000

    Update page.jsx

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,7 +1,7 @@
-import Menu from "@/components/Menu/Menu"
+import Menu from "../../components/Menu/Menu"
 import styles from "./singlePage.module.css"
 import Image from "next/image"
-import Comments from "@/components/comments/comments"
+import Comments from "../../components/comments/Comments"
 
 const SinglePage = () => {
   return (
